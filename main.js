1f7b6c686ce469eeccabfcb8d044014d811e8c78
Yehimi Navarro <128760337+YehimiN@users.noreply.github.com>
2023-04-04 20:25:45 -0600

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('<span style="color: #113759;"> Ing. Robótica y desarroladora de paginas web')
+  .typeString('<span style="color: #113759;"> Ing. Robótica y desarroladora de sitios web')
  .pauseFor(200)
   .deleteChars(10)
   .start();
